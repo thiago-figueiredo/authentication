84ed2e1cdbae2037d882c0faf1ebe43a101669e1
thiago-figueiredo <tfigueiredo@gmail.com>
2021-05-14 16:47:21 -0300

    Adding contact ID to Hasura claims.

--- src/functions/make-access-token.ts
+++ src/functions/make-access-token.ts
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
 
+import { users } from "../data/users.js";
+
 const options = {
   algorithm: "RS512",
   expiresIn: "24h",
@@ -11,20 +13,33 @@ const secretOrPrivateKey = process.env.PRIVATE_OR_SECRET_KEY;
 export const makeAccessToken = ({ email }) => {
   const payload = { email };
 
-  const { claims, namespace } = getHasuraClaims();
+  const userData = getUserData({ email });
+
+  if (userData === undefined) {
+    throw new Error("User not found.");
+  }
+
+  const { claims, namespace } = getHasuraClaims({ userData });
 
   payload[namespace] = claims;
 
   return jwt.sign(payload, secretOrPrivateKey, options);
 };
 
-const getHasuraClaims = () => {
+const getHasuraClaims = ({ userData }) => {
   return {
     claims: {
       "x-hasura-allowed-roles": ["admin"],
       "x-hasura-default-role": "admin",
       "x-hasura-role": "admin",
+      "x-hasura-contact-id": userData.contact_id,
     },
     namespace: "https://hasura.io/jwt/claims",
   };
+};
+
+const getUserData = ({ email }) => {
+  const userData = users.find((user) => user.email === email);
+
+  return userData;
 };
