ecf2ad96058ff2ee42e5e52f64e99d63458526ef
thiago-figueiredo <tfigueiredo@gmail.com>
2021-05-14 16:46:30 -0300

    User data array to simulate database access.

---
+++ src/data/users.ts
@@ -0,0 +1,44 @@
+export const users = [
+  {
+    contact_id: "609d9480260f8f9c8700448b",
+    email: "guilherme",
+    name: "Guilherme Maia",
+    initials: "GM",
+  },
+  {
+    contact_id: "609d948d0b6c656d5b143e19",
+    email: "ilya",
+    name: "Ilya Eremin",
+    initials: "IE",
+  },
+  {
+    contact_id: "609d949373c76ef6c7e131e9",
+    email: "owen",
+    name: "Owen McGettrick",
+    initials: "OM",
+  },
+  {
+    contact_id: "609d949ab74e4cb037d3878e",
+    email: "rodrigo",
+    name: "Rodrigo Pecci Acorse",
+    initials: "RA",
+  },
+  {
+    contact_id: "609d94a0b0ae8b03f49fb3fc",
+    email: "thiago",
+    name: "Thiago Figueiredo",
+    initials: "TF",
+  },
+  {
+    contact_id: "609e7d3a734e3f89952df601",
+    email: "andre",
+    name: "André Peixoto",
+    initials: "AP",
+  },
+  {
+    contact_id: "609d9472c4c0441250191dd5",
+    email: "anton",
+    name: "Anton Chainikov",
+    initials: "AC",
+  },
+];
